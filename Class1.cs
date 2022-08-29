eadfc36009b52f0c8b9f8dea5ecf50114630e356
maximus2k18 <muraweov.m@yandex.ru>
2022-08-29 23:59:47 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿namespace Z_C.ClassLibrary1
+{
+    public class Class1
+    {
+        public static string Checkwin(int[,] matrix)
+        {
+            if (matrix[0, 0] + matrix[0, 1] + matrix[0, 2] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[1, 0] + matrix[1, 1] + matrix[1, 2] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[2, 0] + matrix[2, 1] + matrix[2, 2] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[0, 0] + matrix[1, 1] + matrix[2, 2] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[0, 2] + matrix[1, 1] + matrix[2, 0] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[0, 0] + matrix[1, 0] + matrix[2, 0] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[0, 1] + matrix[1, 1] + matrix[2, 1] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[0, 2] + matrix[1, 2] + matrix[2, 2] == 3)
+            {
+                return "Zeros win!";
+            }
+            if (matrix[0, 0] + matrix[0, 1] + matrix[0, 2] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[1, 0] + matrix[1, 1] + matrix[1, 2] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[2, 0] + matrix[2, 1] + matrix[2, 2] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[0, 0] + matrix[1, 1] + matrix[2, 2] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[0, 2] + matrix[1, 1] + matrix[2, 0] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[0, 0] + matrix[1, 0] + matrix[2, 0] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[0, 1] + matrix[1, 1] + matrix[2, 1] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[0, 2] + matrix[1, 2] + matrix[2, 2] == 12)
+            {
+                return "Сrosses win!";
+            }
+            if (matrix[0, 0] + matrix[0, 1] + matrix[0, 2] + matrix[1, 0] + matrix[1, 1] + matrix[1, 2] + matrix[2, 0] + matrix[2, 1] + matrix[2, 2] == 24)
+            {
+                return "Draw!";
+            }
+            return "";
+        }
+    }
+}
